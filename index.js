930df239658bcf8b9e1dda56a27f8b49c0f64493
Brian <brian.mwangi2@student.moringaschool.com>
2024-04-11 14:41:47 +0300

    adds files

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function hasTargetSum(array, target) {
+
   // Write your algorithm here
+function hasTargetSum(arr, target) {
+    // Set to keep track of seen numbers
+    let seenNumbers = new Set();
+
+    // Iterate through each number in the array
+    for (let num of arr) {
+        // Calculate the difference needed to reach the target
+        let diff = target - num;
+
+        // Check if the difference is already in the seenNumbers set
+        if (seenNumbers.has(diff)) {
+            return true; // Found a pair that adds up to the target
+        }
+
+        // Add the current number to the seenNumbers set
+        seenNumbers.add(num);
+    }
+
+    // If no pair was found
+    return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
